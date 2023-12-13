469edc18df1ff573c5bbdf79fcfb796ad360d8f8
hakangleissman <hakan.gleissman@gmail.com>
2023-12-13 20:22:02 +0100

    Initial commit from gatsby: (https://github.com/hakangleissman/gatsby-with-contentful.git)

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+ 
+const IndexPage = () => (
+  <Layout>
+    <Link to="/portfolio">Se min portfolio</Link>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage;
